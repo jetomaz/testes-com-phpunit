3ee3977d5ae50ebff6176dbde6fdfc65a213e6ac
José Eduardo Tomaz <jetomaz@gmail.com>
2019-04-27 15:17:47 -0300

    testes com Data Provider

--- src/CodeEducation/Math.php
+++ src/CodeEducation/Math.php
@@ -5,6 +5,10 @@ namespace CodeEducation;
 class Math{
 
     public function soma($x, $y){
+        if(!is_numeric($x) or !is_numeric($y)){
+            throw new \InvalidArgumentException("Os valores nao sao numericos");
+        }
+
         return $x + $y;
     }
 
--- tests/src/CodeEducation/MathTest.php
+++ tests/src/CodeEducation/MathTest.php
@@ -9,6 +9,25 @@ class MathTest extends \PHPUnit_Framework_TestCase{
         $this->assertInstanceOf("CodeEducation\Math", new \CodeEducation\Math);
     }
 
+    public function somaProvider(){
+        return [
+            [2,2,4],
+            [10,10,20],
+            [10,15,25],
+            [15,15,30],
+        ];
+    }
+
+    /**
+     * @dataProvider somaProvider
+     */
+    public function testVerificaSeConsegueSomarComProvider($x, $y, $resultado){
+        $math = new \CodeEducation\Math;
+        
+        $res = $math->soma($x,$y);
+        $this->assertEquals($resultado, $res);
+    }
+
     public function testVerificaSeConsegueSomar(){
         $math = new \CodeEducation\Math;
         $resultado = $math->soma(10,12); // sabemos que o resultado deve ser 22
@@ -18,6 +37,18 @@ class MathTest extends \PHPUnit_Framework_TestCase{
         $resultado = $math->soma(2,3); // sabemos que o resultado deve ser 5
         $this->assertEquals(5, $resultado);
 
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testVerificaSeConsegueSomarQuandoNaoNumerico(){
+        $math = new \CodeEducation\Math;
+        
+        $resultado = $math->soma(10,"Isto he um teste"); 
+        $this->assertEquals(22, $resultado);
+
+        
     }
 
 }
